cfb18026c742dc0af8050f39c175875b787d9d9a
jenkeyx <megas1200812@ya.ru>
2022-01-23 23:38:31 +0300

    HTTP error handling functions added

--- src/components/chipsContainer/ChipsContainer.js
+++ src/components/chipsContainer/ChipsContainer.js
@@ -1,41 +1,49 @@
 import "./chips-container.css";
 import PokemonViewer from "../pokemonViewer/PokemonViewer";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Chip } from "@mui/material";
-import { API_URL } from "../../App";
+import React, {useEffect, useState} from "react";
+import {Chip} from "@mui/material";
+import {API_URL} from "../../App";
 
 function ChipsContainer() {
-  const [pokemons, setPokemons] = useState({});
-  const [selectedPokemon, setSelectedPokemon] = useState();
+    const [pokemons, setPokemons] = useState({});
+    const [selectedPokemon, setSelectedPokemon] = useState();
 
-  useEffect(() => {
-    axios.get(API_URL + "?limit=6").then((response) => {
-      let responseData = response.data.results;
-      setPokemons(responseData);
-      setSelectedPokemon(Array.from(responseData)[0].name);
-    });
-  }, [setPokemons]);
+    useEffect(() => {
+        axios.get(API_URL + "?limit=6").then((response) => {
+            let responseData = response.data.results;
+            setPokemons(responseData);
+            setSelectedPokemon(Array.from(responseData)[0].name);
+        }).catch((error) => {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            }
+        });
+    }, [setPokemons]);
 
-  function handleClick(e, index) {
-    setSelectedPokemon(index);
-  }
+    function handleClick(e, index) {
+        setSelectedPokemon(index);
+    }
 
-  return (
-    <div className={"chips-container"}>
-      <div className={"chips-wrap"}>
-        {Array.from(pokemons).map((pokemon) => (
-          <Chip
-            label={pokemon.name}
-            key={pokemon.name}
-            color={"primary"}
-            onClick={(e) => handleClick(e, pokemon.name)}
-          />
-        ))}
-      </div>
-      <PokemonViewer selectedPokemon={selectedPokemon} />
-    </div>
-  );
+    return (
+        <div className={"chips-container"}>
+            <div className={"chips-wrap"}>
+                {Array.from(pokemons).map((pokemon) => (
+                    <Chip
+                        label={pokemon.name}
+                        key={pokemon.name}
+                        color={"primary"}
+                        onClick={(e) => handleClick(e, pokemon.name)}
+                    />
+                ))}
+            </div>
+            <PokemonViewer selectedPokemon={selectedPokemon}/>
+        </div>
+    );
 }
 
 export default ChipsContainer;
--- src/components/pokemonViewer/PokemonViewer.js
+++ src/components/pokemonViewer/PokemonViewer.js
@@ -1,75 +1,83 @@
 import "./pokemon-viewer.css";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Typography } from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Typography} from "@mui/material";
 import capitalizeFirstLetter from "../../helpers/capitalizeFirstLetter";
 import returnImgScale from "../../helpers/returnImgScale";
-import { API_URL } from "../../App";
+import {API_URL} from "../../App";
 import PropTypes from "prop-types";
 
 function PokemonViewer(props) {
-  const statsStyles = { fontWeight: 500, fontSize: "17px", lineHeight: "150%" };
+    const statsStyles = {fontWeight: 500, fontSize: "17px", lineHeight: "150%"};
 
-  const stubPokemonInfo = {
-    name: "select pokemon",
-    sprites: {
-      front_default: "",
-    },
-    moves: [],
-    id: "",
-    height: "",
-    stats: [{}, { base_stat: "" }],
-  };
+    const stubPokemonInfo = {
+        name: "select pokemon",
+        sprites: {
+            front_default: "",
+        },
+        moves: [],
+        id: "",
+        height: "",
+        stats: [{}, {base_stat: ""}],
+    };
 
-  const [pokemonInfo, setPokemonInfo] = useState(stubPokemonInfo);
-  const [isLoaded, setIsLoaded] = useState(null);
+    const [pokemonInfo, setPokemonInfo] = useState(stubPokemonInfo);
+    const [isLoaded, setIsLoaded] = useState(null);
 
-  useEffect(() => {
-    if (props.selectedPokemon) {
-      axios.get(API_URL + props.selectedPokemon).then((response) => {
-        setPokemonInfo(response.data);
-        setIsLoaded(true);
-      });
-    }
-  }, [props.selectedPokemon]);
+    useEffect(() => {
+        if (props.selectedPokemon) {
+            axios.get(API_URL + props.selectedPokemon).then((response) => {
+                setPokemonInfo(response.data);
+                setIsLoaded(true);
+            }).catch((error) => {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                }
+            });
+        }
+    }, [props.selectedPokemon]);
 
-  let imgScale = returnImgScale(pokemonInfo.height);
+    let imgScale = returnImgScale(pokemonInfo.height);
 
-  return (
-    <div className={"pokemon-viewer"}>
-      <Typography
-        sx={{ fontWeight: "bold", lineHeight: "100%" }}
-        variant={"h3"}
-      >
-        {isLoaded ? capitalizeFirstLetter(pokemonInfo.name) : "Pokemon"}
-      </Typography>
-      <div className={"sprite"}>
-        {isLoaded ? (
-          <img
-            alt={"pokemon"}
-            src={pokemonInfo.sprites.front_default}
-            height={imgScale}
-          />
-        ) : null}
-      </div>
-      <div className={"stats"}>
-        <Typography sx={statsStyles}>
-          {`Снялся в ${pokemonInfo.moves.length} сериях`}
-        </Typography>
-        <Typography sx={statsStyles}>{`Id: ${pokemonInfo.id}`}</Typography>
-        <Typography sx={statsStyles}>
-          {`height: ${pokemonInfo.height}`}
-        </Typography>
-        <Typography sx={statsStyles}>
-          {`attack: ${pokemonInfo.stats[1].base_stat}`}
-        </Typography>
-      </div>
-    </div>
-  );
+    return (
+        <div className={"pokemon-viewer"}>
+            <Typography
+                sx={{fontWeight: "bold", lineHeight: "100%"}}
+                variant={"h3"}
+            >
+                {isLoaded ? capitalizeFirstLetter(pokemonInfo.name) : "Pokemon"}
+            </Typography>
+            <div className={"sprite"}>
+                {isLoaded ? (
+                    <img
+                        alt={"pokemon"}
+                        src={pokemonInfo.sprites.front_default}
+                        height={imgScale}
+                    />
+                ) : null}
+            </div>
+            <div className={"stats"}>
+                <Typography sx={statsStyles}>
+                    {`Снялся в ${pokemonInfo.moves.length} сериях`}
+                </Typography>
+                <Typography sx={statsStyles}>{`Id: ${pokemonInfo.id}`}</Typography>
+                <Typography sx={statsStyles}>
+                    {`height: ${pokemonInfo.height}`}
+                </Typography>
+                <Typography sx={statsStyles}>
+                    {`attack: ${pokemonInfo.stats[1].base_stat}`}
+                </Typography>
+            </div>
+        </div>
+    );
 }
 
 PokemonViewer.propTypes = {
-  selectedPokemon: PropTypes.string,
+    selectedPokemon: PropTypes.string,
 };
 
 export default PokemonViewer;
